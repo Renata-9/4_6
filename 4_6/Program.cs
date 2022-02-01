feb1446aea4144d87b84197248ece325334eb7fc
Renata <Renata@DESKTOP-AQ3CSEL>
2022-02-01 10:48:13 +0500

    Добавьте файлы проекта.

---
+++ 4_6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите размер массива: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int S = N - 1;
+            Console.WriteLine("Введите индекс который хотите удалить: ");
+            int K = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[N];
+            int[] mas2 = new int[N];
+            if (K > 0)
+            {
+                if (K < N)
+                {
+                    for (int i = 0; i < N; i++)
+                    {
+                        Console.Write("Введите данные в индекс массива: ");
+                        mas[i] = Convert.ToInt32(Console.ReadLine());
+
+                        if (i == K)
+                        {
+
+                        }
+                        else
+                        {
+                            mas2[i] = i;
+                            Console.WriteLine(" " + mas[i]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
